88aa8a2d6576f872f08bdee65cf0cab14b4ddd9e
sxmar05 <sxm220387@UTdallas.edu>
2025-04-18 16:53:39 -0500

    logout button add

--- nomad/src/pages/ProfilePage.js
+++ nomad/src/pages/ProfilePage.js
@@ -80,6 +80,13 @@ const ProfilePage = () => {
             Find Places
           </Link>
         </div>
+
+        <Link 
+          to="/" 
+          className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Log Out
+        </Link>
         
         <div className="bg-white shadow overflow-hidden sm:rounded-lg">
           <div className="px-4 py-5 sm:px-6">
