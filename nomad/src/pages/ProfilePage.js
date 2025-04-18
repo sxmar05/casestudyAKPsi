0b23d772661c23c4d625dee5920facb23c88b71e
sxmar05 <sxm220387@UTdallas.edu>
2025-04-18 15:58:22 -0500

    sneha final update 4/18

--- nomad/src/pages/ProfilePage.js
+++ nomad/src/pages/ProfilePage.js
@@ -74,8 +74,9 @@ const ProfilePage = () => {
           <h2 className="text-2xl font-bold text-gray-900">Your Profile</h2>
           <Link 
             to="/places" 
-            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-lg text-white bg-blue-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
+            
             Find Places
           </Link>
         </div>
